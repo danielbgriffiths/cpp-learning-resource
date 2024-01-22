c82669ee4785c1e43dd623b502b3481625681e39
danielbgriffiths <danielbgriffiths@protonmail.com>
2024-01-22 07:56:14 -0500

    feat: integrating cmake naively

--- cpp-learning-resource/algorithms/reverse-array/reverse-array.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef reverse_array_hpp
-#define reverse_array_hpp
-
-#include <cstdio>
-#include <vector>
-
-std::vector<int> execute(std::vector<int> list);
-
-#endif /* reverse_array_hpp */
---
+++ libs-algorithms/reverse-array.hpp
@@ -0,0 +1,9 @@
+#ifndef reverse_array_hpp
+#define reverse_array_hpp
+
+#include <cstdio>
+#include <vector>
+
+std::vector<int> reverse_array_memory(std::vector<int> list);
+
+#endif /* reverse_array_hpp */
